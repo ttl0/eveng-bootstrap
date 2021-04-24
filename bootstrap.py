2d7432a60fe7fcfe9451d7ce9f6aeba1c2d4b304
Diego <monasteriosd@gmail.com>
2021-04-24 16:29:21 -0400

    Added threading aswell as flow control to start at any stage

--- bootstrap.py
+++ bootstrap.py
@@ -3,8 +3,9 @@ import time
 import re
 from netmiko import ConnectHandler
 from evengsdk.client import EvengClient
+from threading import Thread
 
-def nxos_provision(host, port, tbr, commands):
+def nxos_provision(host, port, tbr, commands, hostname):
     '''
     Provision nxos device using telnet for eve-ng.
     host: IP address of the EVE-NG server 
@@ -26,13 +27,20 @@ def nxos_provision(host, port, tbr, commands):
 
     # We wait for the device to present us with the
     # option to skip POAP - Power On Auto Provisioning
+    print("{} - Stage 0 - Waiting for device to boot".format(hostname)) 
     while loading_stage1:
         output = net_connect.read_channel()
         if 'skip' in output:
             net_connect.write_channel('skip\r')
             loading_stage1 = False
+        elif 'loader >' in output:
+            net_connect.write_channel('boot nxos.9.2.2.bin\r')
+        elif 'switch#' in output or 'config)#' in output or 'login:' in output:
+            loading_stage1 = False
         time.sleep(tbr)
-    print("Stage 1 configuration completed - skip POAP")
+        net_connect.write_channel('\r')
+
+    print("{} - Stage 1 - Device booted and SOAP skipped".format(hostname))
 
     # We wait for the login prompt to appear so we can
     # log into the device. Default credentials for N9K
@@ -44,21 +52,27 @@ def nxos_provision(host, port, tbr, commands):
             time.sleep(tbr)
             net_connect.write_channel('\r')
             loading_stage2 = False
+        elif '#' in output:
+            loading_stage2 = False
         time.sleep(tbr)
-    print("Stage 2 configuration completed - Logging Completed")
+    print("{} - Stage 2 - Logged in device. Sending commands".format(hostname))
 
     # We wait for the login process to complete and then
     # iterate over all the commands provided and send them
     # one by one.
     while loading_stage3:
         output = net_connect.read_channel()
+        net_connect.write_channel('\r')
         if 'switch#' in output:
             for command in commands:
                 net_connect.write_channel(command + '\r')
                 time.sleep(tbr)
             loading_stage3 = False
+        elif 'config)#' in output:
+            loading_stage3 = False
         time.sleep(tbr)
-    print("Stage 3 configuration completed - Commands Sent")
+    print("{} - Stage 3 - Device configured.".format(hostname))
+
 
 def nxos_command(host, port, tbr, command):
     '''
@@ -83,7 +97,19 @@ def extract_ip(text):
     '''
     return re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', text).group() 
 
-    
+def mt_print(msg):
+    with PRINT_LOCK:
+        print(msg)
+
+def run_mt(mt_function, q, **kwargs):
+    num_threads = min(NUM_THREADS, len(BREAKOUT_PORTS))
+
+    for i in range(num_threads):
+        thread_name = f'Thread-{i}'
+        worker = Thread(name=thread_name, target=mt_function, args=(q, kwargs))
+        worker.start()
+        
+    q.join()    
 
 if __name__ == "__main__":
     # Provide lab name and credentials to log in EVE-NG
@@ -112,10 +138,11 @@ if __name__ == "__main__":
     # Value: Telnet port of the device 
     devices = {}
     for data in result.items():
-        name = data[1]['name']
-        url = data[1]['url'].split(':')[-1]
-        # Have to add 128 for the port to match properly, not sure why
-        devices[name] = str(url)
+        if data[1]['template'] == 'nxosv9k':
+            name = data[1]['name']
+            url = data[1]['url'].split(':')[-1]
+            # Have to add 128 for the port to match properly, not sure why
+            devices[name] = str(url)
 
     print("The Following devices will be provisioned:")
     for hostname in devices:
@@ -132,24 +159,36 @@ if __name__ == "__main__":
                'ip add dhcp', 'username admin password admin role priv-15',
                'hostname ' + hostname
                ]
-        print("Starting with {} on port {}".format(hostname, port))
-        nxos_provision(eveng, port, tbr, commands)
+        print("Starting thread for {} on port {}".format(hostname, port))
+
+        thread = Thread(target = nxos_provision, args = (eveng, port, tbr,
+            commands, hostname))
+        thread.start()
+        #nxos_provision(eveng, port, tbr, commands, hostname)
 
         # Sleep 10 seconds to make sure we get a DHCP address
-        time.sleep(30)
+    thread.join()
+
+    print("Waiting for DHCP..")
+    time.sleep(10)
+    for hostname, port in devices.items():
         command = 'sh ip int b vrf management'
         output = nxos_command(eveng, port, tbr, command)
-        ip = extract_ip(output)
-        print("{} completed.".format(hostname))
+        try:
+            ip = extract_ip(output)
+            devices[hostname] = ip
+            print("{} completed.".format(hostname))
+        except:
+            print("ERROR! {} could not get a DHCP address".format(hostname))
 
         # We rewrite our devices dictionary to store the IP address of
         # the device as Value instead of the port number as follows:
         # Key: Name of the device as defined in EVE-NG
         # Value: IP address of the device 
-        devices[hostname] = ip
 
     # Creates a local file called "hosts" to use as an ansible inventory file
     with open("hosts", "a+") as f:
         for hostname, ip in devices.items():
             f.write("{}    ansible_host={}\n".format(hostname, ip))
+
 
